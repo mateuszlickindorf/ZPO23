a202c6f92c67e06cccacb42f0d72b0194d3b8109
MateuszLickindorf <lic.mateusz@gmail.com>
2023-10-22 02:11:31 +0200

    Fix reference string checker logic
    - Reset the 'wrong' count for each comparison to ensure that previous wrong inputs don't influence the later outcomes

--- Lab1/Ex2.java
+++ Lab1/Ex2.java
@@ -7,7 +7,6 @@ public class Ex2 {
         System.out.print("Podaj łańcuch referencyjny: ");
         String reference = scanner.nextLine().toLowerCase();
 
-        int wrong = 0;
         while (true) {
             System.out.print("Podaj łańcuch do porównania: ");
             String compare = scanner.nextLine().toLowerCase();
@@ -15,6 +14,8 @@ public class Ex2 {
             if (compare.equals("stop")) {
                 break;
             }
+
+            int wrong = 0;
 
             for (int i = 0; i < compare.length(); i++) {
                 int id = reference.indexOf(compare.charAt(i));
