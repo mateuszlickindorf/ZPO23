77aab0f88331669236ebbd413c3a2c7f0a775461
MateuszLickindorf <lic.mateusz@gmail.com>
2023-10-22 02:26:43 +0200

    Fix hex color converter
    - Added a check to ensure the HEX color code is exactly 6 characters long.
    - Corrected the parsing of the green component by using s3 instead of s2.
    - Corrected the printf statement, where the variables g and b were switched up.

--- Lab1/Ex3.java
+++ Lab1/Ex3.java
@@ -9,14 +9,18 @@ public class Ex3 {
 
         scanner.close();
 
+        if (k.length() != 6) {
+            System.out.println("Kod HEX został źle podany, musi składać się z 6 znaków.") ;
+            return;
+        }
         String s1 = k.substring(0, 2);
         String s2 = k.substring(2, 4);
         String s3 = k.substring(4, 6);
 
         int r = Integer.parseInt(s1, 16);
         int g = Integer.parseInt(s2, 16);
-        int b = Integer.parseInt(s2, 16);
-        System.out.printf("(" + r + "," + b + "," + g + ")");
+        int b = Integer.parseInt(s3, 16);
+        System.out.printf("(" + r + "," + g + "," + b + ")");
 
     }
 }
