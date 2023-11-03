f6e00fad1acf9e9ca524dade17b51d39994a22f3
MateuszLickindorf <lic.mateusz@gmail.com>
2023-11-04 00:44:33 +0100

    Create a default constructor and methods describing arythmetic operations possible on complexNumber class
    - Created a default constructor
    - Created getter methods for module and argument
    - Created methods regarding arythmetic operations

--- Lab2/src/complexNumber.java
+++ Lab2/src/complexNumber.java
@@ -1,5 +1,43 @@
-public class complexNumber {
-    /***
-     * Class representing a vector in complex plane
-     */
+
+/**
+ * Klasa reprezentująca wektor w przestrzeni zespolonej
+ */
+public class complexNumber extends Vector2D {
+
+    public complexNumber(double x, double y) {
+        super(x, y);
+    }
+    public double getModule() {
+        return Math.sqrt(getX() * getX() + getY() * getY());
+    }
+
+    public double getArgument() {
+        return Math.atan2(getY(), getX());
+    }
+
+    public complexNumber add(complexNumber other) {
+        return new complexNumber(this.getX() + other.getX(), this.getY() + other.getY());
+    }
+
+    public complexNumber subtract(complexNumber other) {
+        return new complexNumber(this.getX() - other.getX(), this.getY() - other.getY());
+    }
+
+    public complexNumber multiply(complexNumber other) {
+        double realPart = this.getX() * other.getX() - this.getY() * other.getY();
+        double imaginaryPart = this.getX() * other.getY() + this.getY() * other.getX();
+        return new complexNumber(realPart, imaginaryPart);
+    }
+
+    public complexNumber divide(complexNumber other) {
+        double denominator = other.getX() * other.getX() + other.getY() * other.getY();
+        double realPart = (this.getX() * other.getX() + this.getY() * other.getY()) / denominator;
+        double imaginaryPart = (this.getY() * other.getX() - this.getX() * other.getY()) / denominator;
+        return new complexNumber(realPart, imaginaryPart);
+    }
+
+
+
+
+
 }
